716d410d4452188eed80ea1217ed1a70c26a2242
hayapo <39045817+hayapo@users.noreply.github.com>
2025-02-19 00:06:21 +0900

    update meta

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,8 +3,8 @@ import Timer from "~/features/timer/component/Timer";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Segment Pomodoro Timer" },
+    { name: "description", content: "ポモドーロタイマーを広告フリーで使いたい人による、ポモドーロタイマーを広告フリーで使いたい人のための、ポモドーロタイマーを広告フリーで使えるサイト" },
   ];
 };
 
