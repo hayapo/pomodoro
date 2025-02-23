32d323392ab67d450e9d599747729285434f3109
hayapo <39045817+hayapo@users.noreply.github.com>
2025-02-23 15:57:40 +0900

    chore

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "node:path";
-import tailwindConfig from "tailwind.config";
 
 declare module "@remix-run/node" {
   interface Future {
