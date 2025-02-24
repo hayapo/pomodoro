c88721f68f6798c1991d1b2b5ab7cc05ba12ce11
hayapo <39045817+hayapo@users.noreply.github.com>
2025-02-25 00:53:14 +0900

    to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { vitePlugin as remix } from "@remix-run/dev";
+import { vitePlugin as remix, cloudflareDevProxyVitePlugin } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "node:path";
@@ -26,6 +26,7 @@ export default defineConfig(({ mode }) => {
 				},
 			}),
 			tsconfigPaths(),
+			cloudflareDevProxyVitePlugin(),
 		],
 		resolve: {
 			alias: {
