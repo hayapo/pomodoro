3f12ad184a07395299bb623d356bfa3bf3c4188e
hayapo <39045817+hayapo@users.noreply.github.com>
2025-02-24 20:15:03 +0900

    ちゃんとしたカスタムフックに

--- app/features/timer/hooks/usePomodoroText.ts
+++ app/features/timer/hooks/usePomodoroText.ts
@@ -1,11 +1,18 @@
-import type { TimerState } from '~/features/timer/states/timerAtom';
+import { timerAtom, type TimerState } from '~/features/timer/states/timerAtom';
 import { useAtomValue } from 'jotai';
 import { settingsAtom } from '~/features/customize/states/settingsAtom';
+import { useMemo } from 'react';
 
-export const usePomodoroText = (timer: TimerState) => {
+export const usePomodoroText =  () => {
+	const timer = useAtomValue(timerAtom);
 	const settings = useAtomValue(settingsAtom);
-	if (timer.paused || typeof settings.showPomodoroText === 'undefined') {
-		return null;
+	const pomodoroText = useMemo(() => {
+		if (timer.paused || typeof settings.showPomodoroText === 'undefined') {
+			return null;
+		}
+		return timer.pomodoroState === 'focus' ? 'モクモク中 💭' : '休憩中 😌';
+	}, [timer, settings])
+	return {
+		pomodoroText
 	}
-	return timer.pomodoroState === 'focus' ? 'モクモク中 💭' : '休憩中 😌';
 };
