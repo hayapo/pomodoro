0d550447e9885af45d75ec0f5c24095792f97361
hayapo <39045817+hayapo@users.noreply.github.com>
2025-02-24 22:16:24 +0900

    開発サーバーと本番でデフォルトのタイマー分数を分けた

--- app/features/pomodoro/constants.ts
+++ app/features/pomodoro/constants.ts
@@ -1,2 +1,2 @@
-export const DEFAULT_FOCUS_TIMER_MINUTE = 0.1;
-export const DEFAULT_REST_TIMER_MINUTE = 0.05;
+export const DEFAULT_FOCUS_TIMER_MINUTE = import.meta.env.PROD ? 25 : 1;
+export const DEFAULT_REST_TIMER_MINUTE = import.meta.env.PROD ? 5 :1;
